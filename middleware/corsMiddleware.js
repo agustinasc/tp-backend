c78d0f3ad6a80aaafbb20195a9040bfe5e34ecaf
agustinasc <agustina.santillancastro@gmail.com>
2025-05-07 17:48:57 -0300

    corrigiendo CORS

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 // Este middleware habilita CORS para todas las rutas
 const corsOptions = {
-    origin: '*', 
+    origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
